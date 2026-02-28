10cf79af7c5e4556c938d5163ad614899a08b3df
Neelachala Nayak <nnayak0403@gmail.com>
2026-02-28 17:01:01 +0530

    add github actions ci pipeline

--- app.py
+++ app.py
@@ -1,5 +1,13 @@
+from flask import Flask
+
+app = Flask(__name__)
+
 def predict_price(area):
     return area * 5000
 
+@app.route('/predict/<int:area>')
+def predict(area):
+    return {'area': area, 'predicted_price': predict_price(area)}
+
 if __name__ == "__main__":
-    print(predict_price(1000))
+    app.run(debug=True)
